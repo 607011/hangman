314c5c1e8b84bf5f71a62c31a60d57184d20402c
Oliver Lau <ola@ct.de>
2016-04-04 18:17:11 +0200

    Better difficulty levels.

--- js/hangman.js
+++ js/hangman.js
@@ -5,7 +5,10 @@
         'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
         'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'ä', 'ö', 'ü', 'ß'
     ];
-    var Verbs = [];
+    var wordsByDifficulty = [];
+    var difficulty = 10;
+    var minDifficulty;
+    var maxDifficulty;
     var word = undefined;
     var wordChars = [];
     var selectedChars = [];
@@ -81,13 +84,15 @@
         }
         else {
             if (c === ' ') {
-                newGame();
+                newGame(difficulty);
             }
         }
     }
 
 
-    function newGame() {
+    function newGame(newDifficulty) {
+        difficulty = newDifficulty;
+        document.location.hash = '#difficulty=' + difficulty;
         selectedChars = [];
         badChars = [];
         mistakes = 0;
@@ -96,8 +101,8 @@
         $('#message').removeClass('won').empty();
         $('[id^=mistake-]').addClass('invisible');
         $('#bad-characters').text('bislang keine ;-)');
-        var wordLen = Math.floor(Math.random() * (Verbs.length - 4)) + 4;
-        word = Verbs[wordLen][Math.floor(Math.random() * Verbs[wordLen].length)];
+        var dIdx = difficulty - 1 + minDifficulty;
+        word = wordsByDifficulty[dIdx][Math.floor(Math.random() * wordsByDifficulty[dIdx].length)];
         wordChars = word.split('');
         update();
     }
@@ -114,13 +119,50 @@
             method: 'GET',
             type: 'text/plain',
             success: function (data) {
-                $.each(data.split("\n"), function (i, word) {
-                    if (typeof Verbs[word.length] === 'undefined') {
-                        Verbs[word.length] = [];
-                    }
-                    Verbs[word.length].push(word);
+                var words = data.split("\n");
+                var histo = {};
+                var nChars = 0;
+                $.each(words, function (i, word) {
+                    $.each(word.split(''), function (i, c) {
+                        if (typeof histo[c] === 'undefined') {
+                            histo[c] = 0;
+                        }
+                        ++histo[c];
+                        ++nChars;
+                    });
                 });
-                newGame();
+                for (var i in histo) {
+                    histo[i] = 1 - histo[i] / nChars;
+                }
+                wordsByDifficulty = [];
+                minDifficulty = Number.MAX_VALUE;
+                maxDifficulty = Number.MIN_VALUE;
+                $.each(words, function (i, word) {
+                    var singleChars = [];
+                    var allChars = word.split('');
+                    $.each(allChars, function (i, c) {
+                        if (singleChars.indexOf(c) < 0) {
+                            singleChars.push(c);
+                        }
+                    });
+                    var d = 0;
+                    $.each(singleChars, function (i, c) {
+                        d += Math.exp(histo[c]);
+                    });
+                    d = Math.floor(d * singleChars.length / allChars.length);
+                    wordsByDifficulty[d] = wordsByDifficulty[d] || [];
+                    wordsByDifficulty[d].push(word);
+                    minDifficulty = Math.min(d, minDifficulty);
+                    maxDifficulty = Math.max(d, maxDifficulty);
+                });
+                var selectEl = $('#difficulties');
+                for (var i in wordsByDifficulty) {
+                    selectEl.append($('<option></option>').attr('value', i).text(i - minDifficulty + 1));
+                }
+                selectEl.change(function (e) {
+                    newGame(parseInt($(this).val()) - minDifficulty + 1);
+                });
+                selectEl.val(difficulty).change();
             }
         })
     }
