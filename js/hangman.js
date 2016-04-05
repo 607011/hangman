bab709a43cde0ccdafae0a68cc8d588a175e8275
Oliver Lau <ola@ct.de>
2016-04-05 11:57:48 +0200

    Added German nouns.

--- js/hangman.js
+++ js/hangman.js
@@ -1,10 +1,7 @@
 ﻿var Hangman = (function ($, window) {
     'use strict';
 
-    var AllowedChars = [
-        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
-        'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'ä', 'ö', 'ü', 'ß'
-    ];
+    var AllowedChars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'ä', 'ö', 'ü', 'ß'];
     var wordsByDifficulty = [];
     var difficulty = 5;
     var minDifficulty;
@@ -17,6 +14,7 @@
     var MaxMistakes = 7;
     var cheated = false;
     var endOfGame = false;
+
 
     function flash() {
         $('body').addClass('flash');
@@ -56,7 +54,10 @@
 
     function onKeyPressed(e) {
         var c = String.fromCharCode(e.charCode).toLowerCase();
-        if (!endOfGame) {
+        if (c === ' ') {
+            newGame(difficulty);
+        }
+        else if (!endOfGame) {
             if (c === '?') {
                 cheated = true;
                 c = wordChars.find(function(wc) {
@@ -79,11 +80,6 @@
                 update();
             }
         }
-        else {
-            if (c === ' ') {
-                newGame(difficulty);
-            }
-        }
     }
 
 
@@ -101,14 +97,20 @@
         var dIdx = difficulty - 1 + minDifficulty;
         var wIdx = Math.floor(Math.random() * wordsByDifficulty[dIdx].length)
         word = wordsByDifficulty[dIdx][wIdx];
-        wordChars = word.split('');
+        wordChars = word.toLowerCase().split('');
+        $('#word').removeClass();
+        $('#bad-characters').removeClass();
+        if (word[0].toUpperCase() === word[0]) {
+            $('#word').addClass('noun');
+            $('#bad-characters').addClass('noun');
+        }
         update();
     }
 
 
     function wordsLoaded(data) {
         var words = data.split("\n");
-        $('#n-verbs').text(words.length);
+        $('#n-words').text(words.length);
         var histo = {};
         var nChars = 0;
         words.forEach(function (word) {
@@ -118,9 +120,6 @@
                 ++nChars;
             });
         });
-        for (var i in histo) {
-            histo[i] = 1 - histo[i] / nChars;
-        }
         wordsByDifficulty = [];
         minDifficulty = Number.MAX_VALUE;
         maxDifficulty = Number.MIN_VALUE;
@@ -132,10 +131,10 @@
                     charset.push(c);
                 }
             });
-            var d = 0
-            charset.forEach(function(c) {
-                d += Math.exp(histo[c]);
-            });
+            var d = charset.reduce(
+                function sumDifficulty(previousValue, currentValue) {
+                    return previousValue + Math.exp(1 - histo[currentValue] / nChars);
+                }, 0);
             d = Math.floor(d * charset.length / allChars.length / 3);
             wordsByDifficulty[d] = wordsByDifficulty[d] || [];
             wordsByDifficulty[d].push(word);
@@ -157,13 +156,13 @@
 
 
     function doInit() {
-        console.log("%c c't %c Hangman v1.0 BETA patch level 3", 'background-color: #1358A3; color: white; font-weight: bold; font-style: italic; font-size: 150%;', 'background-color: white; color: #1358A3; font-weight: bold; font-size: 150%;');
+        console.log("%c c't %c Hangman v1.0.4", 'background-color: #1358A3; color: white; font-weight: bold; font-style: italic; font-size: 150%;', 'background-color: white; color: #1358A3; font-weight: bold; font-size: 150%;');
         console.log("%cCopyright © 2016 Oliver Lau <ola@ct.de>, Heise Medien GmbH & Co. KG.\nAlle Rechte vorbehalten.", 'color: #1358A3; font-weight: bold;');
         $(window).on({
             keypress: onKeyPressed
         });
         $.ajax({
-            url: 'data/de-verben.txt',
+            url: 'data/de-alle.txt',
             method: 'GET',
             type: 'text/plain',
             success: wordsLoaded
