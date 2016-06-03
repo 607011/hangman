b3cd81c8a778ca33e165148f6dde0d51cadf2ddd
Oliver Lau <ola@ct.de>
2016-06-03 18:18:24 +0200

    Updated.

--- js/hangman.js
+++ js/hangman.js
@@ -3,9 +3,9 @@
 // All rights reserved.
 
 var Hangman = (function ($, window) {
-  'use strict';
+  "use strict";
 
-  var Exclamations = ['Yay', 'Prima', 'Super', 'Klasse', 'Gratulation', 'Wouw', 'Toll', 'Dufte', 'Ausgezeichnet', 'Gewonnen'];
+  var Exclamations = ["Yay", "Prima", "Super", "Klasse", "Gratulation", "Wouw", "Toll", "Dufte", "Ausgezeichnet", "Gewonnen"];
   var MaxMistakes = 7;
 
   var allWords = [];
@@ -19,83 +19,87 @@ var Hangman = (function ($, window) {
 
 
   function showMessageContainer() {
-    $('#message-container').removeClass('invisible');
-    $('#message').append('<p><button>Weiter</button></p>');
+    $("#message-container").removeClass("invisible");
+    $("#message").append("<p><button>Weiter</button></p>");
   }
 
 
   function flash() {
-    $('body').addClass('flash');
+    $("body").addClass("flash");
     setTimeout(function () {
-      $('body').removeClass('flash');
+      $("body").removeClass("flash");
     }, 150);
   }
 
 
   function microFlash() {
-    $('body').addClass('micro-flash');
+    $("body").addClass("micro-flash");
     setTimeout(function () {
-      $('body').removeClass('micro-flash');
+      $("body").removeClass("micro-flash");
     }, 250);
   }
 
 
   function update() {
-    var wordEl = $('#word');
+    var wordEl = $("#word");
     var guessed = wordChars.map(function (c) {
       return selectedChars.indexOf(c) < 0
-          ? '_'
+          ? "_"
           : c;
-    }).join('');
+    }).join("");
     wordEl.text(guessed);
-    $('#mistake-' + nMistakes).removeClass('invisible');
-    var msgEl = $('#message');
+    $("#mistake-" + nMistakes).removeClass("invisible");
+    var msgEl = $("#message");
     if (nMistakes === MaxMistakes) {
       endOfGame = true;
-      msgEl.html('<p>Du hast leider verloren!</p>' +
-          '<p>Das gesuchte Wort war &ldquo;' + word + '&rdquo;.</p>');
+      msgEl.html("<p>Du hast leider verloren!</p>" +
+          "<p>Das gesuchte Wort war &ldquo;" + word + "&rdquo;.</p>");
       showMessageContainer();
     }
-    else if (guessed.indexOf('_') < 0) {
+    else if (guessed.indexOf("_") < 0) {
       endOfGame = true;
-      msgEl.addClass('won').html(cheated
-          ? ('<p>Geschafft!</p>' +
-            '<p>&ldquo;' + word + '&rdquo; ist das richtige Wort!</p>' +
-            '<p>Das nächste Mal bekommst du es ohne Hilfe hin, oder?</p>')
-          : ('<p>' + (Exclamations[Math.floor(Math.random() * Exclamations.length)]) + '!</p>' +
-            '<p>&ldquo;' + word + '&rdquo; ist das richtige Wort!</p>'));
+      msgEl.addClass("won").html(cheated
+          ? ("<p>Geschafft!</p>" +
+             "<p>&ldquo;" + word + "&rdquo; ist das richtige Wort!</p>" +
+             "<p>Das nächste Mal bekommst du es ohne Hilfe hin, oder?</p>")
+          : ("<p>" + (Exclamations[Math.floor(Math.random() * Exclamations.length)]) + "!</p>" +
+             "<p>&ldquo;" + word + "&rdquo; ist das richtige Wort!</p>"));
       showMessageContainer();
     }
   }
 
 
   function keyButton(c) {
-    return $('#virtual-keyboard button:contains("' + c + '")');
+    return $("#virtual-keyboard button:contains('" + c + "')");
   }
 
 
   function selectChar(c) {
-    if (wordChars.indexOf(c) >= 0) {
-      if (selectedChars.indexOf(c) < 0) {
-        selectedChars.push(c);
-        keyButton(c).addClass('good');
+    if (c.match(/[a-zäöü]/i)) {
+      if (wordChars.indexOf(c) >= 0) {
+        if (selectedChars.indexOf(c) < 0) {
+          selectedChars.push(c);
+          keyButton(c).addClass("good");
+        }
       }
-    }
-    else {
-      if (badChars.indexOf(c) < 0) {
-        badChars.push(c);
-        ++nMistakes;
-        keyButton(c).addClass('bad');
-        flash();
+      else {
+        if (badChars.indexOf(c) < 0) {
+          badChars.push(c);
+          ++nMistakes;
+          keyButton(c).addClass("bad");
+          flash();
+        }
       }
+      update();
     }
-    update();
   }
 
 
-  function keyButtonClicked() {
-    $.each($(this).text().split(''), function (i, v) {
-      setTimeout(function() { selectChar(v); }, i * 50);
+  function onKeyButtonClicked() {
+    $.each($(this).text().split(""), function (i, v) {
+      setTimeout(function () {
+        selectChar(v);
+      }, i * 50);
     });
   }
 
@@ -107,11 +111,11 @@ var Hangman = (function ($, window) {
 
   function onKeyPressed(e) {
     var c = String.fromCharCode(e.charCode).toLowerCase();
-    if (c === ' ' || e.charCode === 13) {
+    if (c === " " || e.charCode === 13) {
       newGame();
     }
     else if (!endOfGame) {
-      if (c === '?') {
+      if (c === "?") {
         cheated = true;
         c = wordChars.find(function (wc) {
           return selectedChars.indexOf(wc) < 0;
@@ -128,34 +132,34 @@ var Hangman = (function ($, window) {
     nMistakes = 0;
     endOfGame = false;
     cheated = false;
-    microFlash();
-    $('#message').empty();
-    $('#message-container').removeClass().addClass('invisible');
-    $('[id^=mistake-]').addClass('invisible');
     word = allWords[Math.floor(Math.random() * allWords.length)];
-    wordChars = word.toLowerCase().split('');
-    $('#word').removeClass();
-    $('#bad-characters').removeClass();
+    wordChars = word.toLowerCase().split("");
+    $("#word").removeClass();
     if (word[0].toUpperCase() === word[0]) {
-      $('#word').addClass('noun');
-      $('#bad-characters').addClass('noun');
+      $("#word").addClass("noun");
+      $("#bad-characters").addClass("noun");
     }
-    $('#virtual-keyboard button').removeClass('good bad');
+    $("#message").empty();
+    $("#message-container").removeClass().addClass("invisible");
+    $("[id^=mistake-]").addClass("invisible");
+    $("#bad-characters").removeClass();
+    $("#virtual-keyboard button").removeClass("good bad");
+    microFlash();
     update();
   }
 
 
   function wordsLoaded(data) {
     allWords = data.split(/\r\n|\n|\r/).map(function (word) {
-      return word.replace('ß', 'ss');
+      return word.replace("ß", "ss");
     });
-    $('#n-words').text(allWords.length);
+    $("#n-words").text(allWords.length);
     newGame();
   }
 
 
   function newKeypressEvent(charCode) {
-    var e = $.Event('keypress');
+    var e = $.Event("keypress");
     e.keyCode = e.which = e.charCode = charCode;
     return e;
   }
@@ -167,26 +171,24 @@ var Hangman = (function ($, window) {
 
 
   function doInit() {
-    console.log('%c c\'t %c Hangman v1.0.9', 'background-color: #1358A3; color: white; font-weight: bold; font-style: italic; font-size: 150%;', 'background-color: white; color: #1358A3; font-weight: bold; font-size: 150%;');
-    console.log('%cCopyright © 2016 Oliver Lau <ola@ct.de>, Heise Medien GmbH & Co. KG. Alle Rechte vorbehalten.', 'color: #1358A3; font-weight: bold;');
+    console.log("%c c't %c Hangman v1.0.10", "background-color: #1358A3; color: white; font-weight: bold; font-style: italic; font-size: 150%;", "background-color: white; color: #1358A3; font-weight: bold; font-size: 150%;");
+    console.log("%cCopyright © 2016 Oliver Lau <ola@ct.de>, Heise Medien GmbH & Co. KG. Alle Rechte vorbehalten.", "color: #1358A3; font-weight: bold;");
     $(window).on({
       keypress: onKeyPressed
     });
-    $('#virtual-keyboard button').click(keyButtonClicked);
-    $('button#ernstl').click(keyButtonClicked);
-    $('#new-button').click(function () {
-      simulateKeyPress(' ');
+    $("#virtual-keyboard button").click(onKeyButtonClicked);
+    $("button#ernstl").click(onKeyButtonClicked);
+    $("#new-button").click(function () {
+      simulateKeyPress(" ");
     });
-    $('#hint-button').click(function () {
-      simulateKeyPress('?');
+    $("#hint-button").click(function () {
+      simulateKeyPress("?");
     });
-    $('#message-container').click(function () {
-      newGame();
-    });
+    $("#message-container").click(newGame);
     $.ajax({
-      url: 'data/de-alle.txt',
-      method: 'GET',
-      type: 'text/plain',
+      url: "data/de-alle.txt",
+      method: "GET",
+      type: "text/plain",
       success: wordsLoaded
     });
   }
